59a42f5542949f1f786fd49f6b9b7f4f52d460df
Guderisa <gudetamarisa@gmail.com>
2025-07-09 21:43:36 +0900

    addMonster2

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -1,22 +1,54 @@
 package org.example;
 
-import java.util.Random;
-
 public class Monster {
-  String name;
-  int rare;
+  private String name;
+  private int rare;
 
   // コンストラクタ
-  public Monster(String name, int rare) {
-    this.name = name;
+  public Monster(int nameNum, int rare) {
+    if (rare >= 3) {
+      this.name = evolveName(this.summonMonster(nameNum));
+    } else {
+      this.name = this.summonMonster(nameNum);
+    }
     this.rare = rare;
   }
 
-  public String toString() {
-    return this.name + ":Rarity[" + this.rare + "]";
+  // 進化後の名前
+  private static String evolveName(String originalName) {
+    switch (originalName) {
+      // スライム
+      case "Slime":
+        return "King Slime";
+      // サハギン
+      case "Sahagin":
+        return "Tidal Sahagin";
+      // ドラゴン
+      case "Dragon":
+        return "Ancient Dragon";
+      // デュラハン
+      case "Dullahan":
+        return "Chaos Dullahan";
+      // シーサーペント
+      case "Sea Serpent":
+        return "Leviathan";
+      default:
+        return originalName;
+    }
   }
 
-  public void summonMonster(int num) {
-    
+  public String summonMonster(int num) {
+    // スライム, サハギン, ドラゴン, デュラハン, シーサーペント
+    String monsters[] = { "Slime", "Sahagin", "Dragon", "Dullahan", "Sea Serpent" };
+
+    if (num < 0 || num >= monsters.length) {
+      throw new IllegalArgumentException("Invalid number: " + num);
+    }
+    return monsters[num];
+  }
+
+  @Override
+  public String toString() {
+    return this.name + ":Rarity[" + this.rare + "]";
   }
 }
